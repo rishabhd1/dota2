c9166c745109a705ff32598000560eb6ed264032
Rishabh Dubey <rishabhdubey020@gmail.com>
2019-08-28 01:50:26 +0530

    axios get the api url - incomplete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import MatchDetailForm from "./components/MatchDetailForm";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <MatchDetailForm />
     </div>
   );
 }
--- frontend/src/components/MatchDetailForm.js
+++ frontend/src/components/MatchDetailForm.js
@@ -1,27 +1,43 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
-
-const useStyles = makeStyles(theme => ({
-  margin: {
-    margin: theme.spacing(1)
-  }
-}));
+import TextField from "@material-ui/core/TextField";
+import axios from "axios";
 
 class MatchDetailForm extends React.Component {
+  handleMatchIdSubmit = (event, request) => {
+    const matchId = event.target.elements.matchId.value;
+
+    axios
+      .get(`http://127.0.0.1:8000/api/match-details`, {
+        params: { match_id: matchId }
+      })
+      .then(res => console.log(res))
+      .catch(error => console.log(error));
+  };
+
   render() {
     return (
       <div>
-        <form>
+        <form onSubmit={this.handleMatchIdSubmit}>
+          <TextField
+            name="matchId"
+            label="Match ID"
+            margin="normal"
+            variant="outlined"
+          />
+          <br />
           <Button
+            type="submit"
             variant="contained"
             size="large"
             color="primary"
-            className={classes.margin}
           >
-            Large
+            Submit
           </Button>
         </form>
       </div>
     );
   }
 }
+
+export default MatchDetailForm;
